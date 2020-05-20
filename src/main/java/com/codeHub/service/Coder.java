6f40746ae2b5c906044b300a0ec53ccbad0ea034
adhiambovivian <adhiambovivianne@gmail.com>
2020-05-20 07:42:03 +0300

    refactor file class

--- src/main/java/com/codeHub/service/Coder.java
+++ src/main/java/com/codeHub/service/Coder.java
@@ -65,12 +65,6 @@ public class Coder{
         System.out.println("No xters in chinese word: "+schinese+" "+schinese.length());
         //patternMatching();
         //toConsole();
-        readWriteFileInputOutputStream();
-        readWriteFileInputOutputStreamWithBufferWriter();
-        readWriteFileInputOutputStreamBuffer();
-
-
-
 
     }
 
@@ -165,80 +159,5 @@ public class Coder{
 
     }
 
-    /**
-     * FileOutputStream
-     */
-    public  static void readWriteFileInputOutputStream(){
-        try{
-            long start=System.currentTimeMillis();
-            String filePath="/Users/vivian/PERSONAL_PROJECTS/CodeHub/data/";
-            System.out.println(filePath);
-            FileInputStream fileInputStream=new FileInputStream(filePath+"final.txt");
-            FileOutputStream fileOutputStream=new FileOutputStream(filePath+"ouput.txt");
 
-            int data=0;
-            while((data=fileInputStream.read())!=-1) {
-                fileOutputStream.write(((char)data));
-            }
-            fileInputStream.close();
-            fileOutputStream.close();
-        System.out.println("TT without buffer: "+(System.currentTimeMillis()-start));
-        }catch (IOException e){
-            System.out.println("sth went wrong"+e.getMessage()+" "+e.getStackTrace());
-        }finally {
-
-        }
-    }
-
-    public  static void readWriteFileInputOutputStreamWithBufferWriter(){
-        try{
-            long start=System.currentTimeMillis();
-            String filePath="/Users/vivian/PERSONAL_PROJECTS/CodeHub/data/";
-            System.out.println(filePath);
-            FileInputStream fileInputStream=new FileInputStream(filePath+"final.txt");
-            FileOutputStream fileOutputStream=new FileOutputStream(filePath+"ouput.txt");
-            BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(fileOutputStream);
-
-            int data=0;
-            while((data=fileInputStream.read())!=-1) {
-                bufferedOutputStream.write(((char)data));
-            }
-            bufferedOutputStream.flush();
-            bufferedOutputStream.close();
-            fileInputStream.close();
-            fileOutputStream.close();
-            System.out.println("TT without buffer: "+(System.currentTimeMillis()-start));
-        }catch (IOException e){
-            System.out.println("sth went wrong"+e.getMessage()+" "+e.getStackTrace());
-        }finally {
-
-        }
-    }
-
-    public  static void readWriteFileInputOutputStreamBuffer(){
-        try{
-            long start=System.currentTimeMillis();
-            String filePath="/Users/vivian/PERSONAL_PROJECTS/CodeHub/data/";
-            System.out.println(filePath);
-            FileInputStream fileInputStream=new FileInputStream(filePath+"final.txt");
-            BufferedInputStream bufferedInputStream=new BufferedInputStream(fileInputStream);
-            FileOutputStream fileOutputStream=new FileOutputStream(filePath+"ouput.txt");
-            BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(fileOutputStream);
-
-            int data=0;
-            while((data=bufferedInputStream.read())!=-1) {
-                bufferedOutputStream.write(((char)data));
-            }
-            bufferedInputStream.close();
-            bufferedOutputStream.flush();
-            bufferedOutputStream.close();
-            fileInputStream.close();
-            fileOutputStream.close();
-            System.out.println("TT with buffer: "+(System.currentTimeMillis()-start));
-        }catch (IOException e){
-            System.out.println("sth went wrong"+e.getMessage()+" "+e.getStackTrace());
-        }finally {
-
-        }
-    }
 }

2b9232c871f6e09fdd1e71d853d9faed1752ae64
adhiambovivian <adhiambovivianne@gmail.com>
2020-06-10 15:31:54 +0300

    send dnd list via rmi

---
+++ src/main/java/com/codeHub/service/RmiService.java
@@ -0,0 +1,26 @@
+package com.codeHub.service;
+
+import com.codeHub.models.Blacklist;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RmiService extends UnicastRemoteObject implements DNDInterface {
+
+    RmiService() throws RemoteException{}
+
+    public List<Blacklist> getDndList() {
+        List<Blacklist>dndList=new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Blacklist blacklist = new Blacklist("coment", "OPTIN", i, "+2547263632233" + i);
+            dndList.add(blacklist);
+            blacklist=null;
+
+        }
+        return dndList;
+    }
+}
+
+

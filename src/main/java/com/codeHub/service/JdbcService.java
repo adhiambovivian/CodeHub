bfc81e60981a7f90f8e07026e2ab9bb31564af48
adhiambovivian <adhiambovivianne@gmail.com>
2020-07-01 23:32:20 +0300

    store images

--- src/main/java/com/codeHub/service/JdbcService.java
+++ src/main/java/com/codeHub/service/JdbcService.java
@@ -1,5 +1,7 @@
 package com.codeHub.service;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 
@@ -8,21 +10,42 @@ public class JdbcService {
         connectDbCmd("");
     }
 
-    public static void connectDbCmd(String query){
+    public static Connection connectDbCmd(String query){
+        Connection connection=null;
         try {
+            Class.forName("driver");
             //create connection object
-            Connection connection = DriverManager.getConnection("", "user", "pass");
+            connection = DriverManager.getConnection("", "user", "pass");
             //create statement object
             Statement statement = connection.createStatement();
             //execute query
             ResultSet rs = statement.executeQuery(query);
 
-            while (rs.next()) {
-                //do sth
-            }
-            connection.close();
+//            while (rs.next()) {
+//                //do sth
+//            }
+//            connection.close();
 
         }catch (SQLException e){
+            e.printStackTrace();
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void storeImage(){
+        try {
+            Connection connection = connectDbCmd("");
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO media_images (name, id) values(?,?)");
+            ps.setString(1, "wow");
+            FileInputStream fileInputStream = new FileInputStream("path");
+            ps.setBinaryStream(2, fileInputStream);
+            int status = ps.executeUpdate();
+            connection.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }catch (IOException e){
             e.printStackTrace();
         }
     }

ed1a5c94887195697366737f2e4b6fd07c9afe6e
adhiambovivian <adhiambovivianne@gmail.com>
2020-07-01 23:25:13 +0300

    Add oracle db connection

--- src/main/java/com/codeHub/service/JdbcService.java
+++ src/main/java/com/codeHub/service/JdbcService.java
@@ -1,4 +1,29 @@
 package com.codeHub.service;
 
+import java.sql.*;
+import java.util.Properties;
+
 public class JdbcService {
+    public static void jdbcCmd(){
+        connectDbCmd("");
+    }
+
+    public static void connectDbCmd(String query){
+        try {
+            //create connection object
+            Connection connection = DriverManager.getConnection("", "user", "pass");
+            //create statement object
+            Statement statement = connection.createStatement();
+            //execute query
+            ResultSet rs = statement.executeQuery(query);
+
+            while (rs.next()) {
+                //do sth
+            }
+            connection.close();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }

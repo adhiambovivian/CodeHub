15d292a64ca37b16fed19e30691a9e384443a5fc
adhiambovivian <adhiambovivianne@gmail.com>
2020-07-14 18:36:46 +0300

    fetch emp data

--- src/main/java/com/codeHub/service/JdbcService.java
+++ src/main/java/com/codeHub/service/JdbcService.java
@@ -26,22 +26,40 @@ public class JdbcService {
             dbUsername=properties.getProperty("mysql.datasource.username");
             dbPassword=properties.getProperty("mysql.datasource.password");
             dbName=properties.getProperty("mysql.datasource.dbname");
+            System.out.println(dbName+dbUsername+dbPassword);
         }catch (IOException e){
             e.printStackTrace();
         }
         try {
-            Class.forName("com.mysql.jdbc.Driver.");
+           // Class.forName("com.mysql.cj.jdbc.Driver"); //optional
             //create connection object
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+dbName, dbUsername, dbPassword);
         }catch (SQLException e){
-            e.printStackTrace();
-        }catch (ClassNotFoundException e){
             e.printStackTrace();
         }
         return connection;
     }
 
     public static void getEmpList() {
+
+        String query="SELECT first_name, last_name, department, email FROM employees";
+        try {
+            Connection connection = connectDbCmd();
+            //create statement object
+            Statement statement = connection.createStatement();
+            //execute query
+            ResultSet rs = statement.executeQuery(query);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("first_name"));
+            }
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createTable() {
         String query="\n" +
                 "CREATE TABLE IF NOT EXISTS employees (\n" +
                 "first_name varchar(25),\n" +
@@ -49,6 +67,25 @@ public class JdbcService {
                 "department varchar(15),\n" +
                 "email  varchar(50)\n" +
                 ")";
+        try {
+            Connection connection = connectDbCmd();
+            //create statement object
+            Statement statement = connection.createStatement();
+            //execute query
+            ResultSet rs = statement.executeQuery(query);
+            ResultSet insertRs=statement.executeQuery(query);
+
+            while (insertRs.next()) {
+                //do sth
+            }
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createRecord() {
+
         String insertQuery="INSERT INTO employees (first_name, last_name, department, email) \n" +
                 "VALUES ('Lorenz', 'Vanthillo', 'IT', 'lvthillo@mail.com')";
         try {
@@ -56,16 +93,18 @@ public class JdbcService {
             //create statement object
             Statement statement = connection.createStatement();
             //execute query
-            ResultSet rs = statement.executeQuery(query);
             ResultSet insertRs=statement.executeQuery(insertQuery);
 
             while (insertRs.next()) {
                 //do sth
             }
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
+
 
     public static void storeImage(){
         try {

f832581cab6dc487b24163d46fd2771d07f405aa
adhiambovivian <adhiambovivianne@gmail.com>
2020-07-14 17:20:25 +0300

    add mysql driver

--- src/main/java/com/codeHub/service/JdbcService.java
+++ src/main/java/com/codeHub/service/JdbcService.java
@@ -1,32 +1,38 @@
 package com.codeHub.service;
 
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 
 public class JdbcService {
+    static String propertyPath="/Users/vivian/PERSONAL_PROJECTS/CodeHub/src/main/resources/application.properties";
+
     public static void jdbcCmd()
     {
-        connectDbCmd("");
+        getEmpList();
     }
 
-    public static Connection connectDbCmd(String query){
+    public static Connection connectDbCmd(){
         Connection connection=null;
+        String dbUsername=null;
+        String dbPassword=null;
+        String dbName=null;
+        try {
+            Properties properties = new Properties();
+            FileReader reader = new FileReader(propertyPath);
+            properties.load(reader);
+            dbUsername=properties.getProperty("mysql.datasource.username");
+            dbPassword=properties.getProperty("mysql.datasource.password");
+            dbName=properties.getProperty("mysql.datasource.dbname");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
         try {
             Class.forName("com.mysql.jdbc.Driver.");
             //create connection object
-            connection = DriverManager.getConnection("", "user", "pass");
-            //create statement object
-            Statement statement = connection.createStatement();
-            //execute query
-            ResultSet rs = statement.executeQuery(query);
-
-//            while (rs.next()) {
-//                //do sth
-//            }
-//            connection.close();
-
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+dbName, dbUsername, dbPassword);
         }catch (SQLException e){
             e.printStackTrace();
         }catch (ClassNotFoundException e){
@@ -35,9 +41,35 @@ public class JdbcService {
         return connection;
     }
 
+    public static void getEmpList() {
+        String query="\n" +
+                "CREATE TABLE IF NOT EXISTS employees (\n" +
+                "first_name varchar(25),\n" +
+                "last_name  varchar(25),\n" +
+                "department varchar(15),\n" +
+                "email  varchar(50)\n" +
+                ")";
+        String insertQuery="INSERT INTO employees (first_name, last_name, department, email) \n" +
+                "VALUES ('Lorenz', 'Vanthillo', 'IT', 'lvthillo@mail.com')";
+        try {
+            Connection connection = connectDbCmd();
+            //create statement object
+            Statement statement = connection.createStatement();
+            //execute query
+            ResultSet rs = statement.executeQuery(query);
+            ResultSet insertRs=statement.executeQuery(insertQuery);
+
+            while (insertRs.next()) {
+                //do sth
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void storeImage(){
         try {
-            Connection connection = connectDbCmd("");
+            Connection connection = connectDbCmd();
             PreparedStatement ps = connection.prepareStatement("INSERT INTO media_images (name, id) values(?,?)");
             ps.setString(1, "wow");
             FileInputStream fileInputStream = new FileInputStream("path");

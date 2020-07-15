d832c5e40e5176040a07f38e68e321b27c4a1db2
adhiambovivian <adhiambovivianne@gmail.com>
2020-07-15 09:45:53 +0300

    process emp images

--- src/main/java/com/codeHub/service/JdbcService.java
+++ src/main/java/com/codeHub/service/JdbcService.java
@@ -26,6 +26,7 @@ public class JdbcService {
         getRsDBMetadata();
         getDBMetadata();
         storeImage();
+        storeFile();
     }
 
     public static Connection connectDbCmd(){
@@ -71,7 +72,7 @@ public class JdbcService {
             Statement stmt=connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
             ResultSet resultSet=stmt.executeQuery(query);
             //get data for the 3rd row
-            resultSet.absolute(3);
+            resultSet.absolute(1);
             System.out.println(resultSet.getString(1)+" "+resultSet.getString(2));
             connection.close();
         } catch (SQLException e) {
@@ -109,7 +110,7 @@ public class JdbcService {
             statement.setString(3,"TECH");
             statement.setString(4,"voila@gmail.com");
             //execute query
-            int result = statement.executeUpdate(insertQuery);
+            int result = statement.executeUpdate();
             if(result!=0)
                 System.out.println("Created record successfully");
             connection.close();
@@ -165,8 +166,8 @@ public class JdbcService {
             String createTableQ="CREATE TABLE IF NOT EXISTS profile_image \n" +
                     "   (\n" +
                     "id INT AUTO_INCREMENT PRIMARY KEY,\n" +
-                    "profile_photo BLOB NOT NULL,\n" +
-                    "employee_id INT,\n" +
+                    "profile_photo MEDIUMBLOB NOT NULL,\n" +
+                    "employee_id INT UNIQUE ,\n" +
                     "FOREIGN KEY(employee_id)REFERENCES employee(id)\n" +
                     " \n" +
                     "   )";
@@ -179,23 +180,25 @@ public class JdbcService {
 
             PreparedStatement ps = connection.prepareStatement("INSERT INTO profile_image (profile_photo, employee_id) values(?,?)");
             FileInputStream fileInputStream = new FileInputStream(filePath+"profile.png");
+            System.out.println("Size of data: "+fileInputStream.available());
             ps.setBinaryStream(1, fileInputStream,fileInputStream.available());
             ps.setInt(2, 1);
 
             int status = ps.executeUpdate();
-            if(status>0)
+            if(status>0) {
                 System.out.println("Successfully add profile pic");
 
-            //retrieve file
-            PreparedStatement preparedStatement=connection.prepareStatement("SELECT profile_photo,employee_id FROM profile_image");
-            ResultSet rs=preparedStatement.executeQuery();
-            while(rs.next()){
-                Blob blob=rs.getBlob(1);
-                byte data[]=blob.getBytes(1,(int)blob.length());
-                FileOutputStream fileOutputStream=new FileOutputStream(filePath+"pic_output.png");
-                fileOutputStream.write(data);
-                fileOutputStream.close();
-                fileInputStream.close();
+                //retrieve file
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT profile_photo,employee_id FROM profile_image");
+                ResultSet rs = preparedStatement.executeQuery();
+                while (rs.next()) {
+                    Blob blob = rs.getBlob(1);
+                    byte data[] = blob.getBytes(1, (int) blob.length());
+                    FileOutputStream fileOutputStream = new FileOutputStream(filePath + "pic_output.png");
+                    fileOutputStream.write(data);
+                    fileOutputStream.close();
+                    fileInputStream.close();
+                }
             }
             connection.close();
         }catch (SQLException e){
@@ -210,8 +213,8 @@ public class JdbcService {
             String createTableQ="CREATE TABLE IF NOT EXISTS employee_file \n" +
                     "   (\n" +
                     "id INT AUTO_INCREMENT PRIMARY KEY,\n" +
-                    "file_name CLOB NOT NULL,\n" +
-                    "employee_id INT,\n" +
+                    "file_name TEXT NOT NULL,\n" +
+                    "employee_id INT UNIQUE,\n" +
                     "FOREIGN KEY(employee_id)REFERENCES employee(id)\n" +
                     " )";
             createTable(createTableQ);
@@ -229,22 +232,23 @@ public class JdbcService {
             ps.setInt(2, 1);
 
             int status = ps.executeUpdate();
-            if(status>0)
+            if(status>0) {
                 System.out.println("Successfully add doc");
 
-            //retrieve file
-            PreparedStatement preparedStatement=connection.prepareStatement("SELECT file_name,employee_id FROM employee_file");
-            ResultSet rs=preparedStatement.executeQuery();
+                //retrieve file
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT file_name,employee_id FROM employee_file");
+                ResultSet rs = preparedStatement.executeQuery();
 
-            while(rs.next()){
-                Clob blob=rs.getClob(1);
-                Reader reader=blob.getCharacterStream();
-                FileWriter fileWriter=new FileWriter(filePath+"doc_output.pdf");
-                int data=0;
-                while ((data=reader.read())!=-1)
-                fileWriter.write((char)data);
-                fileWriter.close();
-                reader.close();
+                while (rs.next()) {
+                    Clob blob = rs.getClob(1);
+                    Reader reader = blob.getCharacterStream();
+                    FileWriter fileWriter = new FileWriter(filePath + "doc_output.pdf");
+                    int data = 0;
+                    while ((data = reader.read()) != -1)
+                        fileWriter.write((char) data);
+                    fileWriter.close();
+                    reader.close();
+                }
             }
             connection.close();
         }catch (SQLException e){

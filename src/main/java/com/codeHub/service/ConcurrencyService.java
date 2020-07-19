cd81d2f17b0cfb18299d684bf6b3be956708310c
adhiambovivian <adhiambovivianne@gmail.com>
2020-07-19 11:49:16 +0300

    add thread pool to process tasks

--- src/main/java/com/codeHub/service/ConcurrencyService.java
+++ src/main/java/com/codeHub/service/ConcurrencyService.java
@@ -2,49 +2,98 @@ package com.codeHub.service;
 
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Component
 public class ConcurrencyService {
-    public void multithreadCmd(){
-        Printer p=new Printer();
+    public void multithreadCmd() {
+        Printer p = new Printer();
         p.setName("thread-printer1");
         p.start();
         try {
             Thread.sleep(2000);
             p.join();//wait for p1 to finish
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Printer p2=new Printer();
+        Printer p2 = new Printer();
         p2.setPriority(Thread.MIN_PRIORITY);
         p2.start();
         p2.setName("thread-printer2");
 
-        Printer p3=new Printer();
+        Printer p3 = new Printer();
         p3.start();
         p3.setName("thread-printer3");
 
-        Writer writer=new Writer();
-        Thread thread=new Thread(writer);
+        Writer writer = new Writer();
+        Thread thread = new Thread(writer);
         thread.setName("thread-writer");
         thread.setPriority(Thread.MAX_PRIORITY);
         thread.start();
+
+
+        Writer deamonWriter = new Writer();
+        Thread daemonThread = new Thread(deamonWriter);
+        daemonThread.setName("thread-daemon");
+        daemonThread.setDaemon(true);
+
+        threadPool();
     }
-    class Printer extends Thread{
+
+    class Printer extends Thread {
         public void run() {
             for (int i = 0; i < 5; i++) {
-                System.out.println("Index: "+i+" Thread: Wowowowowow. " + Thread.currentThread().getName());
+                System.out.println("Index: " + i + " Thread: Wowowowowow. " + Thread.currentThread().getName());
             }
         }
     }
-    class Writer implements Runnable{
-        public void run(){
-            for(int i=0;i<5;i++)
-                try{
+
+    class Writer implements Runnable {
+        public void run() {
+            for (int i = 0; i < 5; i++)
+                try {
                     Thread.sleep(3000);
-                }catch (InterruptedException e){
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            System.out.println("Runnable:Wowowowowow "+Thread.currentThread().getName());
+            System.out.println("Runnable:Wowowowowow " + Thread.currentThread().getName());
         }
     }
-}
+
+    class WorkerThread implements Runnable {
+        private String message;
+
+        public WorkerThread(String s) {
+            this.message = s;
+        }
+
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + " (Start) message = " + message);
+            processmessage();//call processmessage method that sleeps the thread for 2 seconds
+            System.out.println(Thread.currentThread().getName() + " (End)");//prints thread name
+        }
+
+        private void processmessage() {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+        public void threadPool() {
+            ExecutorService executorService = Executors.newFixedThreadPool(5);
+            for (int i = 0; i <= 10; i++) {
+                Runnable worker = new WorkerThread("" + i);
+                executorService.execute(worker);
+            }
+            executorService.shutdown();
+            while (!executorService.isTerminated()) {
+            }
+            System.out.println("Finito");
+        }
+    }
+
+

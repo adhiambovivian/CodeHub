47b7d5826d8e280bd67cfcbb7d13fbf0e0427267
adhiambovivian <adhiambovivianne@gmail.com>
2021-05-31 23:09:49 +0300

    add missing imports to jms

--- src/main/java/com/codeHub/configs/JMSConfig.java
+++ src/main/java/com/codeHub/configs/JMSConfig.java
@@ -3,26 +3,31 @@ package com.codeHub.configs;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.listener.DefaultMessageListenerContainer;
+import org.springframework.jms.listener.adapter.MessageListenerAdapter;
+
+import javax.jms.ConnectionFactory;
 
 @Configuration
 public class JMSConfig {
 
     private static final Log log = LogFactory.getLog(JMSConfig.class);
 
-    //    @Bean
-    //    public DefaultMessageListenerContainer jmsListener(ConnectionFactory connectionFactory) {
-    //        DefaultMessageListenerContainer jmsListener = new DefaultMessageListenerContainer();
-    //        jmsListener.setConnectionFactory(connectionFactory);
-    //        jmsListener.setDestinationName("rabbit-trader-channel");
-    //        jmsListener.setPubSubDomain(true);
-    //
-    //        MessageListenerAdapter adapter = new MessageListenerAdapter(new Receiver());
-    //        adapter.setDefaultListenerMethod("receive");
-    //
-    //        jmsListener.setMessageListener(adapter);
-    //        return jmsListener;
-    //    }
+        @Bean
+        public DefaultMessageListenerContainer jmsListener(ConnectionFactory connectionFactory) {
+            DefaultMessageListenerContainer jmsListener = new DefaultMessageListenerContainer();
+            jmsListener.setConnectionFactory(connectionFactory);
+            jmsListener.setDestinationName("rabbit-trader-channel");
+            jmsListener.setPubSubDomain(true);
+
+            MessageListenerAdapter adapter = new MessageListenerAdapter(new Receiver());
+            adapter.setDefaultListenerMethod("receive");
+
+            jmsListener.setMessageListener(adapter);
+            return jmsListener;
+        }
 
     protected static class Receiver {
         public void receive(String message) {

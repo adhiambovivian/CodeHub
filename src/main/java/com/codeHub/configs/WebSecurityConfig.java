ac93f5a684fb427fc7fd67f3b68cfcf57ff15efb
adhiambovivian <adhiambovivianne@gmail.com>
2021-06-01 20:34:36 +0300

    exempt actuator endpoint from auth if request is coming from localhost

--- src/main/java/com/codeHub/configs/WebSecurityConfig.java
+++ src/main/java/com/codeHub/configs/WebSecurityConfig.java
@@ -1,18 +1,28 @@
 /* Copyright (C)2021  Vivian */
 package com.codeHub.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().fullyAuthenticated().and().formLogin();
+        http.authorizeRequests()
+                .antMatchers("/actuator/*").hasIpAddress("127.0.0.1")
+                .anyRequest()
+                .fullyAuthenticated()
+                .and()
+                .formLogin();
     }
 
     @Override

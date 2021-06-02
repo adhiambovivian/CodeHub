549df77a026f7283089ee88f569adadb1aca8099
adhiambovivian <adhiambovivianne@gmail.com>
2021-06-02 23:58:00 +0300

    refactor - code reorg

--- src/main/java/com/codeHub/service/jsonService/GoogleJson.java
+++ src/main/java/com/codeHub/service/jsonService/GoogleJson.java
@@ -1,5 +1,5 @@
 /* Copyright (C)2021  Vivian */
-package com.codeHub.service;
+package com.codeHub.service.jsonService;
 
 import com.codeHub.modelViews.BlackListView;
 import com.codeHub.models.Participant;
